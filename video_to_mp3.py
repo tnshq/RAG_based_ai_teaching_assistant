e29513a0549495fe4592ebe88a0d725df2d51207
tnshq <tanishqsohal2004@gmail.com>
2025-11-23 23:39:01 +0530

    video to mp3

--- video_to_mp3.py
+++ video_to_mp3.py
@@ -2,11 +2,11 @@
 import os 
 import subprocess
 
+os.makedirs("audios", exist_ok=True)
 files = os.listdir("videos") 
 for file in files:
-    print(file)
+    if " #" not in file or " ｜ " not in file: continue
     tutorial_number = file.split(" [")[0].split(" #")[1]
-    print(tutorial_number)
-    # file_name = file.split(" ｜ ")[0]
-    # print( tutorial_number,  file_name)
-    # subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
+    file_name = file.split(" ｜ ")[0]
+    print( tutorial_number,  file_name)
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
